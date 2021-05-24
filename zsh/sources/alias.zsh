265d2e1536105c0c94d7b4cac451ad7b95a86128
ymgn9314 <yamagen.private@gmail.com>
2021-05-24 22:58:08 +0900

    alias: speed_check command added

--- zsh/sources/alias.zsh
+++ zsh/sources/alias.zsh
@@ -2,3 +2,7 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 alias mkdir='mkdir -p'
+
+# zsh speed check
+alias speed_check='time ( zsh -i -c exit )'
+alias speed_check_10='for i in $(seq 1 10); do time zsh -i -c exit; done'
